aef2843a76c0489280f2c0ea266e893c12b695a3
valtermartins1301 <valtermartins.1301@gmail.com>
2020-03-16 19:05:06 -0300

    wip

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors({
-  origin: ['/\.netlify\.com$/'], 
+  origin: ['cookie-auth-app-login.netlify.com', 'cookie-auth-app-logged.netlify.com'], 
   credentials: true,
   maxAge: 24 * 3600,
   preflightContinue: true,
